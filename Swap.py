01d07d0db22d3fd5cdfbc5e8ad3d8229e8d203e1
Brett Regnier <bretternestregnier@gmail.com>
2019-08-15 14:18:44 -0600

    cleaning up code

---
+++ Swap.py
@@ -0,0 +1,12 @@
+import krl
+import Data
+import Env
+
+def Main():
+	pass
+	
+	
+	
+	
+if __name__ == "__main__":
+	Main()
--- krl.py
+++ krl.py
@@ -19,6 +19,34 @@ def Load(model, name):
 def Save(model, name):
     model.save_weights(name)
 
+class Karl:
+	def __init__(self, env, memory=5000, training_steps=20000):
+		self._env
+		self._memory = memory
+		self._ts = training_steps
+		self._model = self.CreateModel()
+		
+	def CreateModel(self):
+		self._model = Sequential()
+		self._model.add(Flatten(input_shape=env.observation_space.shape))
+		self._model.add(Dense(units=nb_actions, activation='sigmoid', input_dim=nb_actions))
+		self._model.add(Dense(units=256, activation='relu'))
+		self._model.add(Dense(units=512, activation='relu'))
+		self._model.add(Dense(units=256, activation='relu'))
+		self._model.add(Dense(units=128, activation='relu'))
+		self._model.add(Dense(units=64, activation='relu'))
+		self._model.add(Dense(units=nb_actions, activation='linear'))
+		
+	def Load(self, name):
+		self._model.load_weights(name)
+	
+	def Save(self, name):
+		self._model.save_weights(name)
+	
+		
+	def CreateModel(self):
+		pass
+	
 env = Data()
 nb_actions = env.action_space.n
 
